38b50dfbb58bfbadab756ae8c246ccc48e668ac5
Ethan Halfhide <ethan.halfhide@gmail.com>
2020-10-17 18:44:20 -0400

    commit

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -17,14 +17,54 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-function showPage
+function showPage(list, page) {
+   // create two variables which will represent the index for the first and last student on the page
+   let startIndex = (page parameter * items per page) - items per page
+
+   let endIndex = page parameter * items per page
+   // select the element with a class of `student-list` and assign it to a variable
+   let studentList = document.querySelector('student-list')
+   // set the innerHTML property of the variable you just created to an empty string
+   const content = element.innerHTML('');
+   // loop over the length of the `list` parameter
+   // inside the loop create a conditional to display the proper students
+       // inside the conditional:
+         // create the elements needed to display the student information
+         // insert the above elements
+   for (let i = 0; i < studentList.length; i++) {
+      if (i >= startIndex && i <= endIndex) {
+         create element that displays student info
+         insert elements
+      }
+   }
+      
+     
+ }
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-function addPagination
+function addPagination(list) {
+   // create a variable to calculate the number of pages needed
+ 
+   // select the element with a class of `link-list` and assign it to a variable
+ 
+   // set the innerHTML property of the variable you just created to an empty string
+ 
+   // loop over the number of pages needed
+     // create the elements needed to display the pagination button
+     // insert the above elements
+ 
+   // give the first pagination button a class of "active"
+ 
+   // create an event listener on the `link-list` element
+     // if the click target is a button:
+       // remove the "active" class from the previous button
+       // add the active class to the clicked button
+       // call the showPage function passing the `list` parameter and page to display as arguments
+ }
 
 
 // Call functions
