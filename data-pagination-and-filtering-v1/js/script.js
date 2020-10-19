35268d202ff48e19973cb048a2a742d41b38dd84
Ethan Halfhide <ethan.halfhide@gmail.com>
2020-10-19 14:18:05 -0400

    commit

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -10,7 +10,7 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
+let itemsPerPage = 9
 
 
 /*
@@ -19,27 +19,32 @@ This function will create and insert/append the elements needed to display a "pa
 */
 function showPage(list, page) {
    // create two variables which will represent the index for the first and last student on the page
-   let startIndex = (page parameter * items per page) - items per page
+   let startIndex = (page. * itemsPerPage) - itemsPerPage
 
-   let endIndex = page parameter * items per page
+   let endIndex = page. * itemsPerPage
    // select the element with a class of `student-list` and assign it to a variable
    let studentList = document.querySelector('student-list')
    // set the innerHTML property of the variable you just created to an empty string
    const content = element.innerHTML('');
    // loop over the length of the `list` parameter
+   for (let i = 0; i < studentList.length; i++) {
    // inside the loop create a conditional to display the proper students
+      if (i >= startIndex && i <= endIndex) {
        // inside the conditional:
+       let studentItem =  
+       /* 
+       Dom Elements needed to display student at index, create elements using template literal. 
+       Use bracket notation to access the student object at that index and 
+       dot notation to access the specific properties of that student object */
          // create the elements needed to display the student information
          // insert the above elements
-   for (let i = 0; i < studentList.length; i++) {
-      if (i >= startIndex && i <= endIndex) {
-         create element that displays student info
-         insert elements
+         element.insertAdjacentHTML(position, text);
       }
    }
       
      
  }
+ //showPage(data, 1)
 
 
 /*
